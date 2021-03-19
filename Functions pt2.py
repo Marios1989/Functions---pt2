44441783c23342a7696fa2b4b28aad0f9b011a5d
Marios1989 <68182435+Marios1989@users.noreply.github.com>
2021-03-19 15:49:10 +0200

    Add files via upload

---
+++ Functions pt2.py
@@ -0,0 +1,35 @@
+# Review Functions
+def answer():
+  return 42
+
+# Planning a trip
+def hotel_cost(nights):
+   return 140 * nights
+
+# Getting There
+def hotel_cost(nights):
+  return 140 * nights
+
+def plane_ride_cost(city):
+  if city == "Charlotte":
+    return 183
+  elif city == "Tampa":
+    return 220
+  elif city == "Pittsburgh":
+    return 222
+  elif city == "Los Angeles":
+    return 475
+
+# Transportation
+def rental_car_cost(days):
+  cost = days * 40
+  if days >= 7:
+    cost -= 50
+  elif days >= 3:
+    cost -= 20
+  return cost
+
+# Pull it together
+def trip_cost(city, days):
+  return rental_car_cost(days) + hotel_cost(days - 1) + plane_ride_cost(city)
+
